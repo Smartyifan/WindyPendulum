a9aa0b6e67370b2411b05db50723b23f41527ed2
Smartyifan <Smartyifan@outlook.com>
2015-07-20 21:00:58 +0800

    compile whole project of PID control.

--- Hardware/TIMER/timer.c
+++ Hardware/TIMER/timer.c
@@ -27,8 +27,8 @@ void TIM3_IRQHandler(void)
 void Timer3_Init(u16 arr,u16 psc)
 {
 	RCC->APB1ENR|=1<<1;//TIM3时钟使能    
- 	TIM3->ARR=arr;  //设定计数器自动重装值//刚好1ms    
-	TIM3->PSC=psc;  //预分频器
+ 	TIM3->ARR=arr-1;  //设定计数器自动重装值//刚好1ms    
+	TIM3->PSC=psc-1;  //预分频器
 	TIM3->DIER|=1<<0;   //允许更新中断				  
 	TIM3->CR1|=0x01;    //使能定时器3
   	MY_NVIC_Init(2,3,TIM3_IRQn);//抢占1，子优先级3，组2									 
--- User/main.c
+++ User/main.c
@@ -39,15 +39,24 @@
 **无入口参数
 **无出口参数
 ---------------------------------------------------*/
-void PIDReset(void)
-{
-	x_PendPID.Kp = 10;
-	x_PendPID.Ki = 0;
-	x_PendPID.Kd = 0;
-	x_PendPID.PIDout_H = 10;     //PID计算值输出限制
-	x_PendPID.PIDout_L = -10;     //PID计算值输出限制
-	PIDParamInit(&x_PendPID);
+
+void ParamSet(){
 	
+	/* HC-05 --------------------------------------*/
+	HC05.USARTBASE = USART1;	//使用串口1驱动	
+	
+	/* MPU6050 ------------------------------------*/
+	JY901.USARTBASE = USART2;   //蓝牙陀螺仪即JY901波特率为115200时，输出速率为100HZ,波特率为9600时，输出速率为20HZ
+
+	/* x_PendPID ----------------------------------*/
+	x_PendPID.Kp = 10;			//Kp	
+	x_PendPID.Ki = 0;			//Ki
+	x_PendPID.Kd = 0;			//Kd
+	x_PendPID.PIDout_H = 10;    //PID计算值输出限制
+	x_PendPID.PIDout_L = -10;   //PID计算值输出限制
+	PIDParamInit(&x_PendPID);	//清空输出
+
+	/* y_PendPID ---------------------------------*/
 	y_PendPID.Kp = 0;
 	y_PendPID.Ki = 0;
 	y_PendPID.Kd = 0;
@@ -62,24 +71,35 @@ void PIDReset(void)
   */
 void Initial()
 {
+	/* 参数设置 ----------------------------------------*/
+	ParamSet();
+	
+	/* 中断分组与延时器初始化 ---------------------------*/
 	NVIC_Configuration();	//中断分组2
 	delay_init();
     
-	JTAG_Set(1);                         //关闭JTAG,开启SWD
-	LED_Init();                          //LED初始化
+	/* SWD初始化 ---------------------------------------*/
+// 	JTAG_Set(1);                         //关闭JTAG,开启SWD
+
+	/* 三色LED初始化 -----------------------------------*/
+	LED_Init();                      
 	delay_ms(10);   
-	//HC05初始化，使用串口1驱动
-	HC05.USARTBASE = USART1;		
+	
+	
+	/* HC05初始化 --------------------------------------*/
 	HC05Init(&HC05);	
 	delay_ms(10);
-	//MPU6050初始化，用串口2驱动	
-	JY901.USARTBASE = USART2;      //蓝牙陀螺仪即JY901波特率为115200时，输出速率为100HZ,波特率为9600时，输出速率为20HZ
-	JY901Init(&JY901);    
+	
+	/* MPU6050初始化 ------------------------------------*/	
+	JY901Init(&JY901);   	//用串口2驱动 
 	delay_ms(10);
-	Motor_Init();                        //电机PWM初始化，用定时器4产生PWM波
+	
+	/* 电机PID初始化 ------------------------------------*/
+	Motor_Init();           //电机PWM初始化，用定时器4产生PWM波
 	delay_ms(10);						
-	Timer3_Init(1999,719);                //定时器3初始化，每20ms一次中断，未使能定时器，只有当偏差计算完成时才启动定时器
-	PIDReset();                          //PID参数复位
+	
+	/* 定时器中断初始化 ---------------------------------*/
+	Timer3_Init(1999,719);	//定时器3初始化，每20ms一次中断，未使能定时器，只有当偏差计算完成时才启动定时器
 }
 
 /**
